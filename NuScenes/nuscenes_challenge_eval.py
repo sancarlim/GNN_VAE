343e8e0c8d693099e6bdffddb217ef0ddef4b670
sandracl72 <sandra.carrascol@uah.es>
2021-05-21 09:50:47 +0000

    challenge_eval MTP

--- NuScenes/nuscenes_challenge_eval.py
+++ NuScenes/nuscenes_challenge_eval.py
@@ -1,13 +1,14 @@
 
 import sys
 sys.path.append('../../DBU_Graph')
+from NuScenes.main_GNN_VAE_nuscenes import NUM_MODES
 import dgl
 import torch
 from torch.utils.data import DataLoader
 import os
 os.environ['DGLBACKEND'] = 'pytorch'
 import numpy as np
-from NuScenes.nuscenes_Dataset import nuscenes_Dataset
+from NuScenes.nuscenes_Dataset import nuscenes_Dataset, collate_batch_test
 from models.VAE_GNN import VAE_GNN
 from models.VAE_GATED import VAE_GATED
 from models.scout import SCOUT
@@ -19,18 +20,18 @@ from argparse import ArgumentParser, Namespace
 from utils import str2bool, compute_change_pos
 from nuscenes.eval.prediction.data_classes import Prediction
 import json
-from NuScenes.nuscenes_visualize import collate_batch
 from torchvision import transforms, utils
+from utils import convert_local_coords_to_global
 
 FREQUENCY = 2
 dt = 1 / FREQUENCY
 history = 2
 future = 6
-history_frames = history*FREQUENCY
+history_frames = history*FREQUENCY + 1
 future_frames = future*FREQUENCY
 total_frames = history_frames + future_frames #2s of history + 6s of prediction
-input_dim_model = (history_frames-1)*8 #Input features to the model: x,y-global (zero-centralized), heading,vel, accel, heading_rate, type 
-output_dim = future_frames*2
+input_dim_model = (history_frames-1)*7 #Input features to the model: x,y-global (zero-centralized), heading,vel, accel, heading_rate, type 
+output_dim = future_frames*2 + 1
 base_path = '/home/sandra/PROGRAMAS/DBU_Graph/NuScenes'
 
 
@@ -53,7 +54,7 @@ class LitGNN(pl.LightningModule):
         self.prediction_scenes = json.load(f)  #Dict with keys "scene_id" : list("instances_samples")
 
         self.rescale_xy=torch.ones((1,1,2), device=self.device)*self.scale_factor
-        
+        self.cnt=0
     
     def forward(self, graph, feats,e_w,snorm_n,snorm_e):
         pred = self.model(graph, feats,e_w,snorm_n,snorm_e)   
@@ -63,7 +64,7 @@ class LitGNN(pl.LightningModule):
         pass
     
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=1, shuffle=False, num_workers=12, collate_fn=collate_batch) 
+        return DataLoader(self.test_dataset, batch_size=1, shuffle=False, num_workers=12, collate_fn=collate_batch_test) 
     
     def training_step(self, train_batch, batch_idx):
         pass
@@ -72,11 +73,11 @@ class LitGNN(pl.LightningModule):
         pass
          
     def test_step(self, test_batch, batch_idx):
-        batched_graph, output_masks,snorm_n, snorm_e, feats, labels_pos, tokens_eval, scene_id, mean_xy, maps = test_batch
+        batched_graph, output_masks,snorm_n, snorm_e, feats, labels_pos, tokens_eval, scene_id, mean_xy, maps, global_feats = test_batch
         
-        last_loc = feats[:,-1:,:2].detach().clone() 
-        feats_vel, labels = compute_change_pos(feats,labels_pos, self.scale_factor)
-        feats = torch.cat([feats_vel, feats[:,:,2:]], dim=-1)[:,1:]
+        #last_loc = feats[:,-1:,:2].detach().clone() 
+        #feats_vel, labels = compute_change_pos(feats,labels_pos, self.scale_factor)
+        #feats = torch.cat([feats_vel, feats[:,:,2:]], dim=-1)[:,1:]
         
         #last_loc = last_loc*self.rescale_xy       
         e_w = batched_graph.edata['w'].float()
@@ -106,6 +107,31 @@ class LitGNN(pl.LightningModule):
                     pred = Prediction(str(instance), str(sample), prediction_all_agents[:,idx], np.ones(1))  #need the pred to have 2d
                     self.challenge_predictions.append(pred.serialize())
 
+        elif self.model_type == 'mtp':
+            pred = self.model(batched_graph, feats,e_w,snorm_n,snorm_e, maps)
+            ##pred=pred.view(feats.shape[0],self.future_frames,-1)
+            
+            mode_prob = pred[:, -NUM_MODES:].clone()
+            desired_shape = (pred.shape[0], NUM_MODES, -1, 2)
+            prediction_all_agents = pred[:, :-NUM_MODES].cpu().numpy().reshape(desired_shape)
+            
+            for j in range(1,labels_pos.shape[1]):
+                prediction_all_agents[:,:,j,:] = np.sum(prediction_all_agents[:,:, j-1:j+1,:],axis=-2) 
+            
+
+            for token in tokens_eval:
+                if str(token[0]+'_'+token[1]) in self.prediction_scenes['scene-'+ str(scene_id).zfill(4)]:
+                    self.cnt += 1
+                    if self.cnt == 1368:
+                        print('stop')
+                    idx = np.where(np.array(tokens_eval)== token[0])[0][0]
+                    instance, sample = token
+                    prediction = prediction_all_agents[idx, :]
+                    for i, pred in enumerate(prediction):
+                        prediction[i] =  convert_local_coords_to_global(pred, global_feats[idx,history_frames-1,:2], global_feats[idx,history_frames-1,2]) + mean_xy
+                    labels = global_feats[idx,history_frames:,:2].cpu().numpy() + mean_xy
+                    preds = Prediction(str(instance), str(sample), prediction, np.ones(3)*1/3)  #need the pred to have 2d
+                    self.challenge_predictions.append(preds.serialize())
 
         else:
             for i in range(25):
@@ -122,7 +148,16 @@ class LitGNN(pl.LightningModule):
                 pred_y = preds[:,:,1].cpu().numpy() + mean_xy[0][1]
                 
                 prediction_all_agents.append(np.stack([pred_x, pred_y],axis=-1))
+                """
+                Stores predictions of Models.
+                Metrics are calculated from Predictions.
 
+                Attributes:
+                    instance: Instance token for prediction.
+                    sample: Sample token for prediction.
+                    prediction: Prediction of model [num_modes, n_timesteps, state_dim].
+                    probabilities: Probabilities of each mode [num_modes].
+                """
                 prediction_all_agents = np.array(prediction_all_agents)
                 for token in tokens_eval:
                     if str(token[0]+'_'+token[1]) in self.prediction_scenes['scene-'+ str(scene_id).zfill(4)]:
@@ -149,14 +184,14 @@ def main(args: Namespace):
         model = VAE_GNN_prior(input_dim_model, args.hidden_dims//args.heads, args.z_dims, output_dim, fc=False, dropout=args.dropout, feat_drop=args.feat_drop,
                         attn_drop=args.attn_drop, heads=args.heads, att_ew=args.att_ew, ew_dims=args.ew_dims, backbone=args.backbone, freeze=args.freeze,
                         bn=(args.norm=='bn'), gn=(args.norm=='gn'))
-    elif args.model_type == 'scout':
-        hidden_dims = round(args.hidden_dims // args.heads)
-        model = SCOUT(input_dim=input_dim_model, hidden_dim=hidden_dims, output_dim=output_dim, heads=args.heads, dropout=args.dropout, bn=(args.norm=='bn'), gn=(args.norm=='gn'),
-                        feat_drop=args.feat_drop, attn_drop=args.attn_drop, att_ew=args.att_ew, ew_dims=args.ew_dims>1, backbone=args.backbone, freeze=args.freeze)
-    else:
+    elif args.model_type == 'vae_gat':
         model = VAE_GNN(input_dim_model, args.hidden_dims//args.heads, args.z_dims, output_dim, fc=False, dropout=args.dropout, 
                         feat_drop=args.feat_drop, attn_drop=args.attn_drop, heads=args.heads, att_ew=args.att_ew, 
                         ew_dims=args.ew_dims, backbone=args.backbone)
+    else:
+        model = SCOUT(input_dim=input_dim_model, hidden_dim=args.hidden_dims, output_dim=output_dim, heads=args.heads, dropout=args.dropout, bn=(args.norm=='bn'), gn=(args.norm=='gn'),
+                        feat_drop=args.feat_drop, attn_drop=args.attn_drop, att_ew=args.att_ew, ew_dims=args.ew_dims>1, backbone=args.backbone, freeze=args.freeze)
+    
     LitGNN_sys = LitGNN(model=model, history_frames=history_frames, future_frames= future_frames, train_dataset=None, val_dataset=None,
                  test_dataset=test_dataset, rel_types=args.ew_dims>1, scale_factor=args.scale_factor, model_type = args.model_type)
       
@@ -175,11 +210,11 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--gpus", type=int, default=1, help="Number of GPUs")
     parser.add_argument("--scale_factor", type=int, default=1, help="Wether to scale x,y global positions (zero-centralized)")
-    parser.add_argument("--ew_dims", type=int, default=1, choices=[1,2], help="Edge features: 1 for relative position, 2 for adding relationship type.")
+    parser.add_argument("--ew_dims", type=int, default=2, choices=[1,2], help="Edge features: 1 for relative position, 2 for adding relationship type.")
     parser.add_argument("--z_dims", type=int, default=25, help="Dimensionality of the latent space")
-    parser.add_argument("--hidden_dims", type=int, default=768)
-    parser.add_argument("--model_type", type=str, default='scout', help="Choose aggregation function between GAT or GATED",
-                                        choices=['vae_gat', 'vae_gated', 'vae_prior','scout'])
+    parser.add_argument("--hidden_dims", type=int, default=512)
+    parser.add_argument("--model_type", type=str, default='mtp', help="Choose aggregation function between GAT or GATED",
+                                        choices=['vae_gat', 'vae_gated', 'vae_prior','scout', 'mtp'])
     parser.add_argument('--freeze', type=int, default=7, help="Layers to freeze in resnet18.")
     parser.add_argument("--norm", type=str, default=None, help="Wether to apply BN (bn) or GroupNorm (gn).")
     parser.add_argument("--dropout", type=float, default=0.1)
@@ -189,8 +224,8 @@ if __name__ == '__main__':
     parser.add_argument('--att_ew', type=str2bool, nargs='?', const=True, default=True, help="Add edge features in attention function (GAT)")
     parser.add_argument('--ckpt', type=str, default=None, help='ckpt path.')   
     parser.add_argument('--nowandb', action='store_true', help='use this flag to DISABLE wandb logging')
-    parser.add_argument("--backbone", type=str, default='resnet', help="Choose CNN backbone.",
-                                        choices=['resnet_gray', 'mobilenet', 'resnet18', 'map_encoder'])
+    parser.add_argument("--backbone", type=str, default='resnet50', help="Choose CNN backbone.",
+                                        choices=['resnet_gray', 'mobilenet', 'resnet18', 'resnet50', 'map_encoder'])
     
     
     hparams = parser.parse_args()
